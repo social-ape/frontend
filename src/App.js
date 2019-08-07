b4c014f4dcce98dd406cb5f90602786147b08991
Nitin verma <canvas.nv@gmail.com>
2019-08-07 14:44:41 +0530

    navbar and basic routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+
+import Navbar from "./components/navbar";
+
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>our app</h1>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
