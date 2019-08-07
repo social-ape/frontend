a5a834553d1a7b0ca00d5c6d2aaefecb6d8b7671
Nitin verma <canvas.nv@gmail.com>
2019-08-07 22:34:55 +0530

    scream cards shown on home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,41 @@
 import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import axios from "axios";
+
+import Scream from "./../components/Scream";
 
 class home extends Component {
+  state = {
+    screams: null
+  };
+  componentDidMount() {
+    axios
+      .get("/screams")
+      .then(result => {
+        this.setState({
+          screams: result.data
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
   render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map(scream => <Scream scream={scream} />)
+    ) : (
+      <p>Loading....</p>
+    );
+
     return (
-      <div>
-        <h1>home page</h1>
-      </div>
+      <Grid container spacing={16}>
+        <Grid item sm={8} xs={12}>
+          <p>{recentScreamsMarkup}</p>
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>...profile</p>
+        </Grid>
+      </Grid>
     );
   }
 }
